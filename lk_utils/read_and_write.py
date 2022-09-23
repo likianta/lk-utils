7c4abdfebf284efb6430938b3cc44635782b3d0e
likianta <likianta@foxmail.com>
2022-09-23 19:14:37 +0800

    Optimize `read_and_write`

--- lk_utils/read_and_write.py
+++ lk_utils/read_and_write.py
@@ -1,31 +1,21 @@
+import typing as t
 from contextlib import contextmanager
 
 
 class T:
-    from typing import List, Literal, Union
-    from typing import BinaryIO, TextIO
-    
+    Content = t.Union[str, t.Iterable[str]]
+    Data = t.Any
     File = str
-    FileMode = Literal['a', 'r', 'rb', 'w', 'wb']
-    FileHandle = Union[TextIO, BinaryIO]
-    
-    PlainFileTypes = Literal['.txt', '.html', '.md', '.rst', '.htm', '.ini']
-    StructFileTypes = Literal['.json', '.json5', '.yaml']
-    BinaryFileTypes = Literal['.xlsx', '.xls', '.pdf']
-    
-    DumpableData = Union[None, dict, list, set, str, tuple]
+    FileMode = t.Literal['a', 'r', 'rb', 'w', 'wb']
+    FileHandle = t.Union[t.TextIO, t.BinaryIO]
+    FileType = t.Literal['auto', 'plain', 'binary',
+                         'json', 'yaml', 'toml', 'pickle']
 
 
 @contextmanager
 def ropen(file: T.File,
           mode: T.FileMode = 'r',
           encoding='utf-8') -> T.FileHandle:
-    """
-    Args:
-        file
-        mode: ('r'|'rb')
-        encoding: ('utf-8'|'utf-8-sig')
-    """
     if 'b' in mode:
         handle = open(file, mode=mode)
     else:
@@ -40,15 +30,6 @@ def ropen(file: T.File,
 def wopen(file: T.File,
           mode: T.FileMode = 'w',
           encoding='utf-8') -> T.FileHandle:
-    """
-    Args:
-        file:
-        mode: ('w'|'a'|'wb')
-            w: 写入前清空原文件已有内容
-            a: 增量写入
-            wb: 以二进制字节流写入
-        encoding ('utf-8'|'utf-8-sig'):
-    """
     if 'b' in mode:
         handle = open(file, mode=mode)
     else:
@@ -69,7 +50,7 @@ def read_file(file: T.File) -> str:
     return content
 
 
-def read_lines(file: T.File, offset=0) -> T.List[str]:
+def read_lines(file: T.File, offset=0) -> t.List[str]:
     """
     References:
         https://blog.csdn.net/qq_40925239/article/details/81486637
@@ -79,30 +60,15 @@ def read_lines(file: T.File, offset=0) -> T.List[str]:
     return out[offset:]
 
 
-def write_file(content: T.Union[iter, list, str, tuple],
-               file: T.File, mode: T.FileMode = 'w', adhesion='\n'):
-    """ 写入文件, 传入内容可以是字符串, 也可以是数组.
-
-    Args:
-        content: 需要写入的文本, 可以是字符串, 也可以是数组. 传入数组时, 会自动
-            将它转换为 "\n" 拼接的文本
-        file: 写入的路径, 建议使用相对路径
-        mode: 写入模式, 有三种可选:
-            a: 增量写入 (默认)
-            w: 清空原内容后写入
-            wb: 在 w 的基础上以比特流的形式写入
-        adhesion: ('\n'|'\t'|...). 拼接方式, 只有当 content 为列表时会用到, 用于
-            将列表转换为文本时选择的拼接方式
-            Example:
-                content = adhesion.join(content)
-                # ['a', 'b', 'c'] -> 'a\nb\nc'
-
-    Refer:
+def write_file(content: T.Content, file: T.File,
+               mode: T.FileMode = 'w', sep='\n'):
+    """
+    ref:
         python 在最后一行追加 https://www.cnblogs.com/zle1992/p/6138125.html
         python map https://blog.csdn.net/yongh701/article/details/50283689
     """
     if not isinstance(content, str):
-        content = adhesion.join(map(str, content))
+        content = sep.join(map(str, content))
     if not content.endswith('\n'):  # add line feed
         content += '\n'
     with wopen(file, mode) as f:
@@ -111,70 +77,53 @@ def write_file(content: T.Union[iter, list, str, tuple],
 
 # ------------------------------------------------------------------------------
 
-def loads(file: T.File, **kwargs) -> T.Union[dict, list, str]:
-    """
-    Args:
-        file:
-        **kwargs:
-            offset: Optional[int]
-    """
-    if file.endswith(('.txt',)):
-        if (offset := kwargs.get('offset', None)) is None:
-            return read_file(file)
-        else:
-            return read_lines(file, offset)
-    
-    if file.endswith(('.htm', '.html', '.md', '.rst', '.txt')):
+def loads(file: T.File, **kwargs) -> T.Data:
+    file_type = _detect_file_type(file)
+    if file_type == 'plain':
         return read_file(file)
-    
-    if file.endswith(('.json', '.json5')):
+    elif file_type == 'binary':
+        with ropen(file, 'rb') as f:
+            return f.read()
+    elif file_type == 'json':
         from json import load as jload
         with ropen(file) as f:
             return jload(f)
-    
-    if file.endswith(('.yaml', '.yml')):  # pip install pyyaml
+    elif file_type == 'yaml':  # pip install pyyaml
         from yaml import safe_load as yload  # noqa
         with ropen(file) as f:
             return yload(f)
-    
-    if file.endswith(('.toml', '.tml')):  # pip install toml
+    elif file_type == 'toml':  # pip install toml
         from toml import load as tload  # noqa
         with ropen(file) as f:
             return tload(f)
-    
-    if file.endswith(('.pkl',)):
+    elif file_type == 'pickle':
         from pickle import load as pload
         with ropen(file, 'rb') as f:
             return pload(f)
-    
-    # unregistered file types, like: .js, .css, .py, etc.
-    return read_file(file)
+    else:
+        # unregistered file types, like: .js, .css, .py, etc.
+        return read_file(file)
 
 
-def dumps(data: T.DumpableData, file: T.File, **kwargs) -> None:
-    """
-    Args:
-        data
-        file
-        **kwargs:
-            mode: TMode, default 'w'
-            adhesion: str, default '\n'
-            pretty_dump: bool, default False
-            
-    """
-    if file.endswith(('.htm', '.html', '.md', '.rst', '.txt')):
-        write_file(data, file, **kwargs)
-    
-    if file.endswith(('.json', '.json5')):
+def dumps(data: T.Data, file: T.File, **kwargs) -> None:
+    file_type = _detect_file_type(file)
+    if file_type == 'plain':
+        write_file(data, file, sep=kwargs.get('sep', '\n'))
+    elif file_type == 'binary':
+        with wopen(file, 'wb') as f:
+            f.write(data)
+    elif file_type == 'json':
         from json import dump as jdump
         with wopen(file) as f:
             jdump(data, f, ensure_ascii=False, default=str,
-                  indent=4 if kwargs.get('pretty_dump', False) else None)
+                  indent=kwargs.get('indent', 4))
             #   ensure_ascii=False
             #       https://www.cnblogs.com/zdz8207/p/python_learn_note_26.html
             #   default=str
             #       when something is not serializble, callback `__str__`.
             #       it is useful to resolve `pathlib.PosixPath`.
+    elif file_type == 'yaml':
+        pass
     
     if file.endswith(('.yaml', '.yml')):  # pip install pyyaml
         from yaml import dump as ydump  # noqa
@@ -198,10 +147,26 @@ def dumps(data: T.DumpableData, file: T.File, **kwargs) -> None:
     return write_file(data, file, **kwargs)
 
 
+def _detect_file_type(filename: str) -> T.FileType:
+    if filename.endswith(('.txt', '.htm', '.html', '.md', '.rst')):
+        return 'plain'
+    elif filename.endswith(('.json', '.json5')):
+        return 'json'
+    elif filename.endswith(('.yaml', '.yml')):  # pip install pyyaml
+        return 'yaml'
+    elif filename.endswith(('.toml', '.tml')):  # pip install toml
+        return 'toml'
+    elif filename.endswith(('.pkl',)):
+        return 'pickle'
+    else:
+        return 'plain'
+        # raise Exception(f'Unknown file type: {filename}')
+
+
 # ------------------------------------------------------------------------------
 
 @contextmanager
-def read(file: T.File, **kwargs) -> T.Union[dict, list, str]:
+def read(file: T.File, **kwargs) -> t.Any:
     """ Open file as a read handle.
     
     Usage:
@@ -216,7 +181,7 @@ def read(file: T.File, **kwargs) -> T.Union[dict, list, str]:
 
 
 @contextmanager
-def write(file: T.File, data: T.Union[dict, list, set] = None, **kwargs):
+def write(file: T.File, data: t.Any = None, **kwargs):
     """ Create a write handle, file will be generated after the `with` block
         closed.
         
