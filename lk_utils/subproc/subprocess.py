40e85c0562c782184f4dc949c1000b0f0298d88d
likianta <likianta@foxmail.com>
2023-03-03 11:20:14 +0800

    Optimize `run_cmd_args`

--- lk_utils/subproc/subprocess.py
+++ lk_utils/subproc/subprocess.py
@@ -1,6 +1,8 @@
+import shlex
 import typing as t
 from subprocess import PIPE
 from subprocess import Popen
+from subprocess import run as sub_run
 
 __all__ = [
     'compose', 'compose_cmd', 'compose_command',
@@ -32,28 +34,36 @@ def compose_command(*args: t.Any, filter_=True) -> t.List[str]:
 
 def run_command_args(
         *args: t.Any, verbose=False,
-        ignore_error=False, filter_=False
+        ignore_error=False, filter_=True,
+        _refmt_args=True
 ) -> str:
-    if filter_:
-        args = tuple(filter(None, map(str, args)))
-    else:
-        args = tuple(map(str, args))
+    """
+    https://stackoverflow.com/questions/58302588/how-to-both-capture-shell
+    -command-output-and-show-it-in-terminal-at-realtime
+    
+    args:
+        _refmt_args: set to False is faster. this is for internal use only.
+    """
+    if _refmt_args:
+        args = compose_command(*args, filter_=filter_)
+    # else:
+    #     assert all(isinstance(x, str) for x in args)
+    
+    if not verbose:
+        sub_run(args, check=not ignore_error)
+        return ''
+    
     proc = Popen(args, stdout=PIPE, stderr=PIPE, text=True)
     
-    if verbose:
-        # https://stackoverflow.com/questions/58302588/how-to-both-capture-shell
-        # -command-output-and-show-it-in-terminal-at-realtime
-        out, err = '', ''
-        for line in proc.stdout:
-            print(':psr', '[dim]{}[/]'.format(
-                line.rstrip().replace('[', '\\[')))
-            out += line
-        for line in proc.stderr:
-            print(':psr', '[red dim]{}[/]'.format(
-                line.rstrip().replace('[', '\\[')))
-            err += line
-    else:
-        out, err = proc.communicate()
+    out, err = '', ''
+    for line in proc.stdout:
+        print(':psr', '[dim]{}[/]'.format(
+            line.rstrip().replace('[', '\\[')))
+        out += line
+    for line in proc.stderr:
+        print(':psr', '[red dim]{}[/]'.format(
+            line.rstrip().replace('[', '\\[')))
+        err += line
     
     if (code := proc.wait()) != 0:
         if not ignore_error:
@@ -69,10 +79,10 @@ def run_command_shell(
         cmd: str, verbose=False,
         ignore_error=False, filter_=False
 ) -> str:
-    import shlex
     return run_command_args(
         *shlex.split(cmd), verbose=verbose,
-        ignore_error=ignore_error, filter_=filter_
+        ignore_error=ignore_error, filter_=filter_,
+        _refmt_args=False
     )
 
 
@@ -85,8 +95,8 @@ class E:
                 return_code: int = None
         ):
             self._args = ' '.join(args)
-            self._response = response
-            self._return_code = str(return_code or 'null')
+            self._resp = response
+            self._code = str(return_code or 'null')
         
         def __str__(self):
             from textwrap import dedent, indent
@@ -97,9 +107,9 @@ class E:
                     response:
                         {response}
             ''').format(
-                code=self._return_code,
+                code=self._code,
                 args=self._args,
-                response=indent(self._response, ' ' * 8).lstrip()
+                response=indent(self._resp, ' ' * 8).lstrip()
             ).strip()
 
 
