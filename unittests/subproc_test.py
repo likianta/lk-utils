8543da8ee718dd389b3a39af0ec111f166c2191f
likianta <likianta@foxmail.com>
2023-03-03 11:56:32 +0800

    Update unittests

--- unittests/subproc_test.py
+++ unittests/subproc_test.py
@@ -2,6 +2,7 @@ from argsense import cli
 from lk_utils import subproc
 
 
+@cli.cmd()
 def test_composing_command():
     print(subproc.compose_cmd('a'))  # -> ['a']
     print(subproc.compose_cmd('a', 'b'))  # -> ['a', 'b']
@@ -12,12 +13,18 @@ def test_composing_command():
     print(subproc.compose_cmd('a', 'b', ('c', '')))  # -> ['a', 'b']
 
 
+@cli.cmd()
 def test_popen_cmd():
-    print(subproc.run_cmd_shell('python -V'))
-    print(subproc.run_cmd_args('python', '-V'))
-    print(subproc.run_cmd_args('python', '-m', 'pip', '-V'))
+    print(subproc.run_cmd_shell('python3 -V'))
+    print(subproc.run_cmd_args('python3', '-V'))
+    print(subproc.run_cmd_args('python3', '-m', 'pip', '-V'))
+    print(':d', 'add verbose')
+    subproc.run_cmd_shell('python3 -V', verbose=True)
+    subproc.run_cmd_args('python3', '-V', verbose=True)
+    subproc.run_cmd_args('python3', '-m', 'pip', '-V', verbose=True)
 
 
+@cli.cmd()
 def test_raise_cmd_error():
     try:
         print(subproc.run_cmd_args('depsland', '-V'))
